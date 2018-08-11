838e3954481570baac7df557b1098fe0b05163a2
Christian Heussy <Christian@CJs-Mac.local>
2018-08-10 17:29:36 -0700

    Test commit of template files to check Git is working.

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+my_list = ['this', 'is', 'a', 'list']
+
+@app.route('/')
+def index():
+    name = 'Christian'
+    some_variable = list(name)
+    pup_dict={'Sammy': 'Doge'}
+    return render_template('basic.html',my_variable=my_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
