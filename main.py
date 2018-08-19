2ce19bbf4fbbb0d051bb51cfb1526f7f900f8310
Christian Heussy <Christian@CJs-Mac.local>
2018-08-19 09:28:17 -0700

    Added index.html. Created generic landing page.

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ app = Flask(__name__)
 
 my_list = ['this', 'is', 'a', 'list']
 
+
 @app.route('/')
 def index():
     name = 'Christian'
     some_variable = list(name)
     pup_dict={'Sammy': 'Doge'}
-    return render_template('basic.html',my_variable=my_list)
+    return render_template('index.html',my_variable=my_list)
 
 
 if __name__ == '__main__':
