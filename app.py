5d22fe1541cb2742d743541b3bd1c13fd9f839a8
Christian Heussy <Christian@CJs-Mac.local>
2018-08-26 19:58:59 -0700

    Added projects page with senior project and racing. Basic placeholders for content.

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ def index():
     pup_dict={'Sammy': 'Doge'}
     return render_template('index.html',my_variable=my_list)
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/senior')
+def senior():
+    return render_template('senior.html')\
+
+@app.route('/racing')
+def race():
+    return render_template('race.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
